04fec78ce5679bea6b840f7735bc29e13e38b60d
Lautaro Matias Lezana Luna <lautaromatias9@gmail.com>
2023-10-29 15:38:37 -0300

    Collection-View-Update

---
+++ UIKit8-UICollectionView/swiftBetaCollectionViewCell.swift
@@ -0,0 +1,58 @@
+//
+//  swiftBetaCollectionViewCell.swift
+//  UIKit8-UICollectionView
+//
+//  Created by Lautaro matias Lezana luna on 21/11/2022.
+//
+
+import UIKit
+
+class swiftBetaCollectionViewCell: UICollectionViewCell {
+    private let swiftBetaStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.layer.borderColor = UIColor.black.cgColor
+        stackView.layer.borderWidth = 1
+        return stackView
+    }()
+    
+    private let deviceImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let deviceNameLabel: UILabel = {
+       let label = UILabel()
+        label.font = .systemFont(ofSize: 24)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        addSubview(swiftBetaStackView)
+        swiftBetaStackView.addArrangedSubview(deviceImageView)
+        swiftBetaStackView.addArrangedSubview(deviceNameLabel)
+        
+        NSLayoutConstraint.activate([
+            swiftBetaStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            swiftBetaStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            swiftBetaStackView.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(model: Device) {
+        deviceImageView.image = UIImage(systemName: model.imageName)
+        deviceNameLabel.text = model.title
+    }
+    
+}
